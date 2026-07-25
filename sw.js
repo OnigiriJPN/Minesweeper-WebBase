6153cd36c3a01ceac0cc606d02380fbf91fbf710
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-25 16:27:43 +0900

    Add service worker caching for pre-defined URLs

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
 const CACHE_NAME="Minesweeper-Cache"
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/win7.html',
+  '/win98.html',
+  '/winxp.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRECACHE_URLS);
+    }).catch((e) => {});
+});
