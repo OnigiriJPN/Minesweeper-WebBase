5de871483c37ec16ea6b3803a092db3920f29938
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-25 21:15:38 +0900

    Improve error handling in service worker

--- sw.js
+++ sw.js
@@ -12,6 +12,27 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(PRECACHE_URLS);
     }).catch((e) => {
-      throw new Error("エラー:" + e.message);
+      throw new Error("エラー0: " + e.message);
+    });
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch((e) => {
+          throw new Error("エラー1: " + e.message);
+        });
+      }).catch((e) => {
+        throw new Error("エラー2: " + e.message);
+      });
+　　　}).catch((e) => {
+      throw new Error("エラー3: " + e.message);
     });
 });
