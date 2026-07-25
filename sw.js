6fa2b5cb23a65eb8523dd6c04dd4d0f4fbb628b8
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-25 20:59:39 +0900

    Handle cache addition errors with a thrown message
    Throw an error with a message on cache addition failure.

--- sw.js
+++ sw.js
@@ -11,5 +11,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(PRECACHE_URLS);
-    }).catch((e) => {});
+    }).catch((e) => {
+      throw new Error("エラー:" + e.message);
+    });
 });
